86ddc2d004e467ec2340482707a54b2db0812028
Mustafa ÖNERYILDIZ <onery.mustafa@gmail.com>
2026-07-19 06:21:40 +0300

    trial_1

--- Internship_WPF_Practices/MainWindow.xaml.cs
+++ Internship_WPF_Practices/MainWindow.xaml.cs
@@ -4,10 +4,30 @@ namespace Internship_WPF_Practices
 {
     public partial class MainWindow : Window
     {
+        bool state = false;
         public MainWindow()
         {
             InitializeComponent();
+            
+            // ButtonState.Text = "Control Panel";
+            // Button.Content = "Stop";
 
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            if (state)
+            {
+                ButtonState.Text = "Stopped";
+                Button.Content = "Start";
+            }
+            else
+            {
+                ButtonState.Text = "Running";
+                Button.Content = "Stop";
+            }
+
+            state = !state;
         }
     }
 }
