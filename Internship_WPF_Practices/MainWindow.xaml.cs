ff32eaa8b6c9105689ea4bad01b5d32d6954b7d6
Mustafa ÖNERYILDIZ <onery.mustafa@gmail.com>
2026-07-19 04:56:38 +0300

    Add project files.

---
+++ Internship_WPF_Practices/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Internship_WPF_Practices
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
