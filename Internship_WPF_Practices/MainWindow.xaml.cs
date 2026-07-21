864ccfe614cd8471c5dec9680a846561cdef128f
onery-mustafa <onery.mustafa@gmail.com>
2026-07-21 15:04:24 +0300

    Trial_1

--- Internship_WPF_Practices/MainWindow.xaml.cs
+++ Internship_WPF_Practices/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.Windows;
 
 namespace Internship_WPF_Practices
 {
@@ -7,6 +8,13 @@ namespace Internship_WPF_Practices
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog FileDialog = new OpenFileDialog();
+            FileDialog.ShowDialog(); // bu metot, dosya açma iletişim kutusunu gösterir ve kullanıcı bir dosya seçene kadar bekler
 
         }
     }
