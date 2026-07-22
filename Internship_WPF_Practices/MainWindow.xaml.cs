1df76e57b5e087d420d75be75b26d86d90fe8028
onery-mustafa <onery.mustafa@gmail.com>
2026-07-22 12:00:22 +0300

    Trial_1

--- Internship_WPF_Practices/MainWindow.xaml.cs
+++ Internship_WPF_Practices/MainWindow.xaml.cs
@@ -6,8 +6,31 @@ namespace Internship_WPF_Practices
     {
         public MainWindow()
         {
-            InitializeComponent();
-
+         // listeyi önceden doldurma:
+        /*  InitializeComponent();
+            IvEntries.Items.Add("A");
+            IvEntries.Items.Add("B");
+            IvEntries.Items.Add("C");
+        */
         }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            IvEntries.Items.Add(txtEntry.Text);
+            txtEntry.Clear();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int index = IvEntries.SelectedIndex; // Seçili öğenin indeksini al
+            IvEntries.Items.RemoveAt(index); // Seçili öğeyi kaldır
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            IvEntries.Items.Clear();
+        }
+
+        
     }
 }
