83883e6663ab2d4da193bfd81bca0df33f72d3d6
onery-mustafa <onery.mustafa@gmail.com>
2026-07-24 17:26:04 +0300

    trial

--- Internship_WPF_Practices/MainWindow.xaml.cs
+++ Internship_WPF_Practices/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Internship_WPF_Practices.View;
 
 namespace Internship_WPF_Practices
 {
@@ -8,6 +9,20 @@ namespace Internship_WPF_Practices
         {
             InitializeComponent();
 
+        }
+
+        private void btnNormal_Click(object sender, RoutedEventArgs e)
+        {
+            // Burada yapmamız gereken ilk şey, normal pencerenin bir örneğini oluşturmak.
+            NormalWindow NormalWindow = new NormalWindow();
+            NormalWindow.Show(); //NormalWindow u gösteriyoruz.
+         }
+
+        private void btnMoal_Click(object sender, RoutedEventArgs e)
+        {
+
+            ModalWindow ModalWindow = new ModalWindow();
+            ModalWindow.ShowDialog();
         }
     }
 }
---
+++ Internship_WPF_Practices/View/NormalWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Internship_WPF_Practices.View
+{
+    public partial class NormalWindow : Window
+    {
+        public NormalWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
