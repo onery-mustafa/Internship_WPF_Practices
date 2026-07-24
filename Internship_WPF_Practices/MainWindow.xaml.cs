5e193470e58a192472e10ce670f8f02b4e47bf77
onery-mustafa <onery.mustafa@gmail.com>
2026-07-24 14:08:33 +0300

    trial

--- Internship_WPF_Practices/MainWindow.xaml.cs
+++ Internship_WPF_Practices/MainWindow.xaml.cs
@@ -9,5 +9,28 @@ namespace Internship_WPF_Practices
             InitializeComponent();
 
         }
+
+        private void Window_MouseLeftButton(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+             Close();
+            //Application.Current.Shutdown();
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if(WindowState == WindowState.Maximized) WindowState = WindowState.Normal;
+            else WindowState = WindowState.Maximized;
+            
+        }
     }
 }
