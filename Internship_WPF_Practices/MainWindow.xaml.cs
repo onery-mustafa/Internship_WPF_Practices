60b2038c11dfb30dbc21e0da59d56b6df2f64f62
onery-mustafa <onery.mustafa@gmail.com>
2026-07-28 14:03:50 +0300

    trial

--- Internship_WPF_Practices/MainWindow.xaml.cs
+++ Internship_WPF_Practices/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace Internship_WPF_Practices
     {
         public MainWindow()
         {
-            //DataContext = this;
+            DataContext = this; // Veri bağlamı özelliği bu arka plan kod dosyasına ayarladık. 
             InitializeComponent();
 
         }
