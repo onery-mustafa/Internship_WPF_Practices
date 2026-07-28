bba423cc1cac98e05ae6806974acd2a1adca5ea5
onery-mustafa <onery.mustafa@gmail.com>
2026-07-28 13:41:33 +0300

    trial

--- Internship_WPF_Practices/MainWindow.xaml.cs
+++ Internship_WPF_Practices/MainWindow.xaml.cs
@@ -26,3 +26,20 @@ namespace Internship_WPF_Practices
         }
     }
 }
+
+
+/*
+ 
+INotifyPropertyChanged Nedir ve Implementasyonu:
+https://gokhan-gokalp.com/wpf-inotifypropertychanged-nedir-ve-implementasyonu/
+
+https://canertosuner.com/post/inotifypropertychanged-nedir-nasil-kullanilir
+
+C# Masterclass 1307 Dependency Properties
+https://www.youtube.com/watch?v=jJZsSiVWDqE
+
+https://www.yazilimkodlama.com/wpf/wpf-baslangicta-acilacak-formu-belirleme/#google_vignette
+
+https://learn.microsoft.com/tr-tr/dotnet/desktop/wpf/data/how-to-implement-property-change-notification
+
+*/
