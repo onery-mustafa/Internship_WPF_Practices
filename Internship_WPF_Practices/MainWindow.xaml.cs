d98417b2bd9fed2f8fe1231a08d29d57a43b72a6
onery-mustafa <onery.mustafa@gmail.com>
2026-08-07 16:20:53 +0300

    trial

--- Internship_WPF_Practices/MainWindow.xaml.cs
+++ Internship_WPF_Practices/MainWindow.xaml.cs
@@ -1,13 +1,31 @@
 ﻿using System.Windows;
+using System.Windows.Threading;
+
 
 namespace Internship_WPF_Practices
 {
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
 
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+
+      
+        }
+        
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+           // timerView.Text = DateTime.Now.ToString("HH:mm:ss");
+
+           // message.Foreground = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Red);
+            
         }
     }
 }
